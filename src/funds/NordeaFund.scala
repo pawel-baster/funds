06498afc8a6f3564ec90ab338978c2155c9bb158
pawel-baster <pb@boligportal.dk>
2012-11-01 07:55:47 +0100

    NordeaFund: unit test passes now.

--- src/funds/NordeaFund.scala
+++ src/funds/NordeaFund.scala
@@ -15,26 +15,30 @@ class NordeaFund(
   override val downloader: Downloader
 ) extends UpdatableFund(shortName, new CurrencyDKK, downloader) {
   var quotes = new HashMap[Date, Double]
+
   def update() = {
     val csvFile = downloader.download("someUrl")
     csvFile.getLines.drop(1).foreach(l => {
       val cols = l.split(" ")
       val date = new SimpleDateFormat("dd-MM-yyy").parse(cols(0))
-      if (getQuoteForDate(date) < 0) {
-        //quotes(date) = cols(1)
-        quotes = quotes + ((date, cols(1).toDouble))
+      if (quotes.get(date) == None) {
+        quotes = quotes + ((date, cols(1).replace(',', '.').toDouble))
       }
     })
-
     quotes.foreach(item => println(item))
   }
   def calculateSellFee(value: Double): Double = value
+
   def calculateBuyFee(value: Double): Double = value
+
   def getQuoteForDate(date: Date): Double = {
     var quote = quotes.get(date)
     if (quote == None) {
       update()
       quote = quotes.get(date)
+    }
+    if (quote == None) {
+      throw new RuntimeException("Missing value for date " + date);
     }
     return quote.getOrElse(-1.0)
   }
--- src/tests/NordeaFundUpdateTest.scala
+++ src/tests/NordeaFundUpdateTest.scala
@@ -1,3 +1,5 @@
+import java.lang.RuntimeException
+import java.text.SimpleDateFormat
 import java.util.Date
 
 /**
@@ -9,12 +11,20 @@ import java.util.Date
  */
 
 val fund1 = new NordeaFund("test1", new MockDownloader("src/tests/fixtures/nordea_export_1.csv"))
-val date1 = new Date // 10-10-2012
-assert(fund1.getQuoteForDate(date1) == 98.76)
+val date1 = new SimpleDateFormat("dd-MM-yyy").parse("10-10-2012")
+val expectedValue1 = 98.76
+assert(fund1.getQuoteForDate(date1) == expectedValue1, "Expected value: " + expectedValue1 + ", got: " + fund1.getQuoteForDate(date1))
 
 // assert throws exception
-val date2 = new Date() // 22-10-2012
-fund1.getQuoteForDate(date2)
+val date2 = new SimpleDateFormat("dd-MM-yyy").parse("22-10-2012")
+try {
+  val result = fund1.getQuoteForDate(date2)
+  assert(false, "Exception not thrown, returned value: " + result)
+} catch {
+  case e: RuntimeException => assert(true, "Exception expected")
+}
 
 val fund2 = new NordeaFund("test2", new MockDownloader("src/tests/fixtures/nordea_export_2.csv")) //mockDownloader
 assert(fund2.getQuoteForDate(date2) == 99.13)
+
+println("OK")
