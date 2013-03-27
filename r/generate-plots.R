e86314c936b7a85ae606720f4b45289a7349dac5
Pawel <pabaster@gmail.com>
2013-03-27 07:42:31 +0100

    working on the plotting script

--- r/generate-plots.R
+++ r/generate-plots.R
@@ -3,9 +3,9 @@ history.full <- read.table("../data/MBankFullExperiment_best_history.csv", sep="
 history.ward <- read.table("../data/WardMBankExperiment_best_history.csv", sep=";", quote="\"")
 history.basic <- read.table("../data/MBankExperiment_best_history.csv", sep=";", quote="\"")
 
-maxy = max(c(history.best.ranked[,2], history.full[,2], history.ward[,2], history.basic[,2])
+maxy = max(c(history.best.ranked[,2], history.full[,2], history.ward[,2], history.basic[,2]))
 
-plot(history.best.ranked[,2], type="l", col="red")
+plot(history.best.ranked[,2], type="l", col="red", ylim=c(0,maxy))
 lines(history.full[,2], type="l", col="blue")
 lines(history.ward[,2], type="l", col="green")
 lines(history.basic[,2], type="l", col="purple")
